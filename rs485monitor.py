3687130f5b189f73e781616ac0e394e85e1d766b
Samir Ahamada <sahamada@aldebaran-robotics.com>
2014-06-05 12:07:26 +0200

    Lotta changes

--- rs485monitor.py
+++ rs485monitor.py
@@ -2,6 +2,7 @@
 from pylibftdi import Device, FtdiError
 from sys import stdout, exit
 from os import system
+import argparse
 
 '''
 Wraps a stream object into an auto-flushing one
@@ -25,22 +26,32 @@ class RS485Monitor:
         self._d.baudrate = baudrate
         self._d.ftdi_fn.ftdi_set_line_property(databits, stopbits, paritymode)
         self._d.flush()
+        self._out = Unbuffered(stdout)
+        self._raw = False
+
+    def raw(self):
+        self._out.write(self._d.read(1))
+
+    def normal(self):
+        d = self._d.read(128)
+        self._out.write('\r')
+        if (len(d) > 0):
+            self._out.write('[' + d + '] (' + str(len(d)) + ')')
 
     def run(self):
         system('clear')
-        print "Monitor started : Baudrate=" + str(self._d.baudrate)
-        out = Unbuffered(stdout)
-        d = ''
-        for i in range(128):
-            d += ' '
+        self._out.write("Monitor started : Baudrate=" + str(self._d.baudrate))
+        if (self._raw):
+            self._out.write(' - [Raw mode]\n')
+        else:
+            self._out.write('\n[] (0)')
 
         while (1):
             try:
-                d = self._d.read(128)
-                for i in range (150):
-                    out.write(' ')
-                out.write('\r')
-                out.write('[' + d + '] (' + str(len(d)) + ')\r')
+                if (self._raw):
+                    self.raw()
+                else:
+                    self.normal()
             except FtdiError as e:
                 print 'Exception caught : ' + e.args[0]
                 print 'Exiting monitor'
@@ -49,6 +60,14 @@ class RS485Monitor:
                print '\r\nExiting monitor'
                exit(0)
 
+
 if __name__ == "__main__":
+    p = argparse.ArgumentParser(description='Monitor FTDI RS485 Rx.')
+    p.add_argument('-r', '--raw',
+                    default = False,
+                    action="store_true",
+                    help='Raw output')
+    args = p.parse_args()
     mon = RS485Monitor()
+    mon._raw = args.raw
     mon.run()
--- spitransmitter.py
+++ spitransmitter.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python
 from mpsse import *
 from sys import stdin, exit
+from time import sleep
+import argparse
 
 class SPITransmitter:
-    def __init__(self, mode = SPI0, frequency = 460000):
+    def __init__(self, mode, freq):
+        self._usermode = mode
+        self._userfreq = freq
         try:
-            self._m = MPSSE(mode, frequency)
+            self._m = MPSSE(mode, freq)
         except Exception as e:
             print '\rCould not start FTDI Device : ' + e.args[0]
             exit(0)
 
     def run(self):
-        print "Transmitter started : Frequency=" + str(self._m.GetClock())
-        a = ord('A') 
+        print "Transmitter started : Mode=" + str(self._usermode) + " | Frequency=" + str(self._m.GetClock())
+        a = ord('A')
+        s = 1
         while (1):
             try:
                 self._m.Start()
                 self._m.Write(chr(a))
-                self._m.Write(chr(a + 1))
+                self._m.Write(chr(a + s))
                 self._m.Stop()
-                a = a + 1 if (a < ord('z')) else ord('A')
+                a += s * 2
+                if (((chr(a) >= 'y') and (s == 1)) or ((chr(a) <= 'b') and (s == -1))):
+                    s = -s
+                    sleep(1)
             except KeyboardInterrupt:
                print '\r\nExiting transmitter'
                self._m.Close()
@@ -34,6 +42,15 @@ class SPITransmitter:
                 exit(1)
 
 if __name__ == "__main__":
-    t = SPITransmitter()
+    p = argparse.ArgumentParser(description='Transmit characters on FTDI SPI.')
+    p.add_argument('-m', '--mode',
+                    type=int,
+                    choices=[SPI0, SPI1, SPI2, SPI3],
+                    default=SPI0,
+                    help='SPI Mode (see pylibmpsse doc for further info)')
+    p.add_argument('-f', '--frequency',
+                    type=int,
+                    default='460000')
+    args = p.parse_args()
+    t = SPITransmitter(args.mode, args.frequency)
     t.run()
-
